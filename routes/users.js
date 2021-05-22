5a136deafcefda9f3967f87ed7eb94601f5e8ed8
EthernalSlayer <guillaume.b.a@protonmail.com>
2021-05-22 19:47:36 +0200

    rafactoring 3

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const UsersController = require('../controllers/users.controller');
 const usersPostValidator = require('../utils/validator/usersPostValidator');
 const usersPutValidator = require('../utils/validator/usersPutValidator');
+const loginValidator = require('../utils/validator/loginValidator');
 
 router
 	.get('/', UsersController.getUsers)
 	.post('/', usersPostValidator, UsersController.postUser)
 	.delete('/:id', UsersController.deleteUser)
 	.put('/:id', usersPutValidator, UsersController.updateUser)
-	.post('/login', UsersController.login);
+	.post('/login', loginValidator, UsersController.login);
 
 module.exports = router;
